933625ed391ec4162d01e1f684d5c0d67a1b2e0e
Romanna-Brych <romannabric@gmail.com>
2025-09-15 13:55:04 +0000

    Deploying to gh-pages from @ Romanna-Brych/goit-js-hw-10@3b7eeb5e4e553ef6309897188f7d212f7127130d 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,14 +1,14 @@
-import"./assets/iziToast-custom-BZimflmy.js";/* empty css                      */import{i as n}from"./assets/vendor-BbbuE1sJ.js";const l=document.querySelector(".form"),u=document.querySelector('input[name="delay"]'),a=document.querySelector('input[value="fulfilled"]');l.addEventListener("submit",m);function m(r){r.preventDefault();const t=u.value;new Promise((o,c)=>{a.checked?setTimeout(()=>{o(t)},t):setTimeout(()=>{c(t)},t)}).then(o=>{n.success({title:"OK",message:`Fulfilled promise in ${o}ms`,position:"topRight",backgroundColor:"#59a10d",titleColor:"#fff",messageColor:"#fff",icon:" ",close:!1,class:"custom-toast",onOpening:function(c,s){const i=s.querySelector(".iziToast-icon");i&&(i.innerHTML=`
+import{s as c}from"./assets/sprite-DztAg9nn.js";/* empty css                      */import{i as n}from"./assets/vendor-BbbuE1sJ.js";const r=document.querySelector(".form"),a=document.querySelector('input[name="delay"]'),m=document.querySelector('input[value="fulfilled"]');r.addEventListener("submit",f);function f(u){u.preventDefault();const i=a.value;new Promise((s,l)=>{m.checked?setTimeout(()=>{s(i)},i):setTimeout(()=>{l(i)},i)}).then(s=>{n.success({title:"OK",message:`Fulfilled promise in ${s}ms`,position:"topRight",backgroundColor:"#59a10d",titleColor:"#fff",messageColor:"#fff",icon:" ",close:!1,class:"custom-toast",onOpening:function(l,o){const t=o.querySelector(".iziToast-icon");t&&(t.innerHTML=`
         <svg class="icon-custom">
-          <use href="./img/sprite.svg#icon-Group"></use>
+          <use href="${c}#icon-Group"></use>
         </svg>`);const e=document.createElement("button");e.classList.add("iziToast-close-custom"),e.innerHTML=`
         <svg class="icon-custom-close">
-          <use xlink:href="./img/sprite.svg#icon-Vector-3"></use>
-        </svg>`,e.addEventListener("click",()=>{n.hide({},s)}),s.appendChild(e)}})}).catch(o=>{n.error({title:"Error",message:`Rejected promise in ${o}ms`,position:"topRight",backgroundColor:" #ef4040",titleColor:"#fff",messageColor:"#fff",icon:" ",close:!1,class:"custom-toast",onOpening:function(c,s){const i=s.querySelector(".iziToast-icon");i&&(i.innerHTML=`
+          <use href="${c}#icon-Vector-3"></use>
+        </svg>`,e.addEventListener("click",()=>{n.hide({},o)}),o.appendChild(e)}})}).catch(s=>{n.error({title:"Error",message:`Rejected promise in ${s}ms`,position:"topRight",backgroundColor:" #ef4040",titleColor:"#fff",messageColor:"#fff",icon:" ",close:!1,class:"custom-toast",onOpening:function(l,o){const t=o.querySelector(".iziToast-icon");t&&(t.innerHTML=`
         <svg class="icon-custom">
-          <use href="./img/sprite.svg#icon-bi_x-octagon"></use>
+          <use href="${c}#icon-bi_x-octagon"></use>
         </svg>`);const e=document.createElement("button");e.classList.add("iziToast-close-custom"),e.innerHTML=`
         <svg class="icon-custom-close">
-          <use xlink:href="./img/sprite.svg#icon-Vector-3"></use>
-        </svg>`,e.addEventListener("click",()=>{n.hide({},s)}),s.appendChild(e)}})}),l.reset()}
+          <use href="${c}#icon-Vector-3"></use>
+        </svg>`,e.addEventListener("click",()=>{n.hide({},o)}),o.appendChild(e)}})}),r.reset()}
 //# sourceMappingURL=2-snackbar.js.map
